346a765f5758994d0f10c7840d0704a0d415d9fb
novicecoder205 <warpick2000@gmail.com>
2024-10-16 16:41:01 +0700

    Shopping store project, started on 12/10, finish on 14/10

---
+++ src/products.js
@@ -0,0 +1,76 @@
+import imageProduct1 from './assets/images/1.png' 
+import imageProduct2 from './assets/images/2.png' 
+import imageProduct3 from './assets/images/3.png' 
+import imageProduct4 from './assets/images/4.png' 
+import imageProduct5 from './assets/images/5.png' 
+import imageProduct6 from './assets/images/6.png' 
+import imageProduct7 from './assets/images/7.png' 
+import imageProduct8 from './assets/images/8.png' 
+
+export const products = [
+    {
+        id: 1,
+        name: 'Hertfoid Upholstered Chair',
+        price: 100,
+        image: imageProduct1,
+        description: 'The Hertfoid Upholstered Chair combines modern elegance with cozy comfort. Featuring a plush, padded seat and backrest, it invites you to sink in and relax. The sleek design is complemented by soft, durable upholstery available in a variety of colors, making it a versatile addition to any room. With its stylish tapered legs and streamlined silhouette, the Hertfoid is perfect for both contemporary and traditional spaces, offering a touch of sophistication to your living room, office, or reading nook.',
+        slug: 'hertfoid-upholstered-chair'
+    },
+    {
+        id: 2,
+        name: 'Abingdon Upholstered Chair Swivel',
+        price: 150,
+        image: imageProduct2,
+        description: 'The Abingdon Upholstered Chair Swivel is the perfect blend of comfort and functionality. This stylish chair features a plush, cushioned seat and back, enveloped in luxurious upholstery that invites you to sit back and unwind. Its smooth swivel mechanism allows for easy movement, making it ideal for social gatherings or cozy reading corners. The chair elegant design, with refined stitching and contemporary legs, enhances any décor while providing a touch of sophistication. Whether in a living room, office, or entertainment space, the Abingdon adds both style and versatility.',
+        slug: 'bingdon-pholstered-chair-swivel'
+    },
+    {
+        id: 3,
+        name: 'Jeses Minimore Modern Style Etta',
+        price: 200,
+        image: imageProduct3,
+        description: 'The Jeses Minimore Modern Style Etta is a striking addition to contemporary interiors, showcasing a perfect fusion of form and function. This chair features a sleek, streamlined design with gentle curves that create an inviting silhouette. Upholstered in soft, durable fabric, it offers a cozy seating experience without compromising on style. The Etta’s minimalist aesthetic is enhanced by understated details, making it a versatile choice for any room, from living spaces to home offices. Available in an array of modern hues, this chair effortlessly elevates your décor while providing a chic spot to relax.',
+        slug: 'jeses-minimore-modern-style-etta'
+    },
+    {
+        id: 4,
+        name: 'JJeses Minimore Modern Style',
+        price: 250,
+        image: imageProduct4,
+        description: 'The Jeses Minimore Modern Style collection embodies contemporary sophistication with a focus on clean lines and functional design. Each piece in this collection features a minimalist aesthetic, combining sleek silhouettes with high-quality materials for a polished look. The thoughtfully crafted upholstery offers both comfort and durability, making it perfect for everyday use. Ideal for modern living rooms, offices, or lounges, the Jeses Minimore collection is designed to enhance any space, bringing an air of elegance and style while ensuring practicality and versatility in your home.',
+        slug: 'eses-minimore-modern-style-zakari'
+    },
+    {
+        id: 5,
+        name: 'Bolanle Upholstered Armchair',
+        price: 300,
+        image: imageProduct5,
+        description: 'The Bolanle Upholstered Armchair is a perfect blend of comfort and chic design, making it an essential addition to any living space. This armchair features plush padding in both the seat and backrest, enveloped in soft, high-quality upholstery for a luxurious feel. Its elegantly curved arms and sturdy frame provide both style and support, while the versatile design fits seamlessly into various décor styles, from modern to traditional. Available in an array of colors and patterns, the Bolanle Armchair is ideal for creating a cozy reading nook or enhancing your living room with a touch of sophistication.',
+        slug: 'corrigan-studio-tufed-upholstered-wide-winback-armchair'
+    },
+    {
+        id: 6,
+        name: 'Jaqueze Upholstered Armchair',
+        price: 350,
+        image: imageProduct6,
+        description: 'The Jaqueze Upholstered Armchair exudes contemporary charm and comfort, making it a standout piece for any room. Featuring a plush, cushioned seat and backrest, this armchair invites you to relax in style. Its sleek lines and tailored upholstery create a sophisticated look, while the gently tapered legs add a touch of elegance. Available in a variety of trendy colors and patterns, the Jaqueze is versatile enough to complement any décor, whether in a living room, office, or reading nook. Perfect for both casual lounging and formal gatherings, it effortlessly blends comfort with modern aesthetics.',
+        slug: 'corrigan-studio-273-wide-armchair'
+    },
+    {
+        id: 7,
+        name: 'Leston Wide Upholstered Fabric',
+        price: 400,
+        image: imageProduct7,
+        description: 'The Leston Wide Upholstered Fabric Chair is a spacious and inviting seating option that combines comfort with stylish design. With its generous width, this chair offers ample space to relax and unwind, making it perfect for lounging or entertaining. The soft, high-quality fabric upholstery not only enhances comfort but also adds a touch of elegance to your decor. Its clean lines and modern silhouette make it a versatile addition to any room, whether its a cozy living area or a chic office space. Available in a range of colors, the Leston chair effortlessly complements various styles, bringing a warm and welcoming vibe to your home.',
+        slug: 'leston-wide-upholstered-fabric'
+    },
+    {
+        id: 8,
+        name: 'Stephanny 27.5" Wide Tufted',
+        price: 450,
+        image: imageProduct8,
+        description: 'The Stephanny 27.5" Wide Tufted Chair is a stylish accent piece that combines comfort with a touch of classic elegance. Its generous width provides ample seating space, while the exquisite tufting detail adds texture and visual interest. Upholstered in soft, durable fabric, this chair is designed for both relaxation and style. The sturdy frame and gracefully curved legs ensure stability and support, making it a perfect choice for living rooms, bedrooms, or entryways. Available in a variety of chic colors, the Stephanny chair effortlessly enhances your decor while offering a cozy spot to sit and unwind.',
+        slug: 'stephanny-275-wide-tufted-armchair'
+    },
+    
+];
